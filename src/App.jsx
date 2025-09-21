1ad4484532f3063f9dce9f936a84880955b9d8c8
Mustafa Keskin <mustafakeskinozel@gmail.com>
2025-09-21 16:58:15 +0300

    Update App.jsx
    sipariş formu import edildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import HomePage from "./pages/HomePage";
+import OrderForm from "./pages/OrderForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState("home");
+  const [orderData, setOrderData] = useState(null);
+
+  const goToForm = () => {
+    setPage("form");
+  };
+
+  const goToConfirmation = (data) => {
+    console.log("Confirmation için veri:", data);
+    setOrderData(data);
+    setPage("confirmation");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {page === "home" && <HomePage goToForm={goToForm} />}
+      {page === "form" && <OrderForm goToConfirmation={goToConfirmation} />}
+      {page === "confirmation" && <h2>Sipariş Onayı buraya gelecek</h2>}
+    </div>
+  );
 }
 
-export default App
+export default App;
